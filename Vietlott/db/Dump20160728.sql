a2ef9e108ee617b4a663d8c7d4a823f8f97eafd4
andy134 <tuananh01499@gmail.com>
2016-07-28 14:14:19 +0700

    db

---
+++ Vietlott/db/Dump20160728.sql
@@ -0,0 +1,151 @@
+CREATE DATABASE  IF NOT EXISTS `lottery_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `lottery_db`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: lottery_db
+-- ------------------------------------------------------
+-- Server version	5.7.12-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `Image_Id` int(10) NOT NULL AUTO_INCREMENT,
+  `Image_Name` varchar(225) NOT NULL,
+  `Image_Path` varchar(225) NOT NULL,
+  `Image_Desc` text,
+  `Upload_Date` datetime DEFAULT NULL,
+  PRIMARY KEY (`Image_Id`),
+  UNIQUE KEY `Image_Id_UNIQUE` (`Image_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pages`
+--
+
+DROP TABLE IF EXISTS `pages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pages` (
+  `Page_Id` int(10) NOT NULL AUTO_INCREMENT,
+  `Page_Name` varchar(225) NOT NULL,
+  `Page_Content` text NOT NULL,
+  `Page_Slug` varchar(225) DEFAULT NULL,
+  `User_Id` int(10) DEFAULT NULL,
+  `Publish_Date` datetime DEFAULT NULL,
+  `Last_Edit` datetime DEFAULT NULL,
+  `Status` int(10) DEFAULT NULL,
+  PRIMARY KEY (`Page_Id`),
+  UNIQUE KEY `Page_Id_UNIQUE` (`Page_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pages`
+--
+
+LOCK TABLES `pages` WRITE;
+/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `Post_Id` int(10) NOT NULL AUTO_INCREMENT,
+  `Post_Name` varchar(255) NOT NULL,
+  `Cat_Id` int(10) DEFAULT NULL,
+  `User_Id` int(10) DEFAULT NULL,
+  `Post_Content` text,
+  `Post_Slug` varchar(225) DEFAULT NULL,
+  `Publish_Date` datetime DEFAULT NULL,
+  `Last_Edit` datetime DEFAULT NULL,
+  `Image` varchar(225) DEFAULT NULL,
+  `Num_Views` int(10) DEFAULT NULL,
+  `Status` int(10) DEFAULT NULL,
+  PRIMARY KEY (`Post_Id`),
+  UNIQUE KEY `Post_Id_UNIQUE` (`Post_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `User_Id` int(10) NOT NULL AUTO_INCREMENT,
+  `First_Name` varchar(100) NOT NULL,
+  `Last_Name` varchar(100) DEFAULT NULL,
+  `Gender` smallint(1) DEFAULT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `User_Role` varchar(255) NOT NULL,
+  `Active_Date` datetime DEFAULT NULL,
+  `Avatar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`User_Id`),
+  UNIQUE KEY `userid_UNIQUE` (`User_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Tuan Anh','Luu',1,'tuananh01499@gmail.com','123','user',NULL,NULL),(2,'Quoc Viet','Nguyen',1,'quocviet@gmail.com','hello','admin','2016-07-27 00:00:00',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-28 14:12:33
