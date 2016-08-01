7dd5f966c8df3c5ba90a36d3d3fe2bf9f4bd1549
andy134 <tuananh01499@gmail.com>
2016-08-01 18:46:03 +0700

    page update'

--- Maxtoto/src/java/com/lottery/model/Page.java
+++ Maxtoto/src/java/com/lottery/model/Page.java
@@ -10,5 +10,111 @@ package com.lottery.model;
  * @author TuanAnh
  */
 public class Page {
+    private int pageId;
+    private String pageName;
+    private String pageContent;
+    private String pageSlug;
+    private int userId;
+    private String lastEdit;
+    private int status;
+
+    /**
+     * @return the pageId
+     */
+    public int getPageId() {
+        return pageId;
+    }
+
+    /**
+     * @param pageId the pageId to set
+     */
+    public void setPageId(int pageId) {
+        this.pageId = pageId;
+    }
+
+    /**
+     * @return the pageName
+     */
+    public String getPageName() {
+        return pageName;
+    }
+
+    /**
+     * @param pageName the pageName to set
+     */
+    public void setPageName(String pageName) {
+        this.pageName = pageName;
+    }
+
+    /**
+     * @return the pageContent
+     */
+    public String getPageContent() {
+        return pageContent;
+    }
+
+    /**
+     * @param pageContent the pageContent to set
+     */
+    public void setPageContent(String pageContent) {
+        this.pageContent = pageContent;
+    }
+
+    /**
+     * @return the pageSlug
+     */
+    public String getPageSlug() {
+        return pageSlug;
+    }
+
+    /**
+     * @param pageSlug the pageSlug to set
+     */
+    public void setPageSlug(String pageSlug) {
+        this.pageSlug = pageSlug;
+    }
+
+    /**
+     * @return the userId
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return the lastEdit
+     */
+    public String getLastEdit() {
+        return lastEdit;
+    }
+
+    /**
+     * @param lastEdit the lastEdit to set
+     */
+    public void setLastEdit(String lastEdit) {
+        this.lastEdit = lastEdit;
+    }
+
+    /**
+     * @return the status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
     
 }
